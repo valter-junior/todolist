773e0c193fa0e3636b97530d0f9b86790e7fce74
Valter Junior <valtrjose.sjr@gmail.com>
2021-04-22 13:58:48 -0300

    Update files

--- front/src/index.js
+++ front/src/index.js
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
