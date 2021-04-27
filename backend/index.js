e90b173fd6c784e7b1a8097d8370278d2484c9db
Valter Junior <valtrjose.sjr@gmail.com>
2021-04-27 13:12:47 -0300

    Update files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const sequelize = require("./db").default;
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,8 +17,8 @@ app.use(express.json());
 app.post("/sign-in", async (req, res) => {
   try {
     const { firstName, lastName, login, password } = req.body;
-    const newTodo = await sequelize.query("INSERT INTO todo (firstName, lastName, login, password) VALUES('${firstName}', '${lastName}', '${login}', '${password}') RETURNING *");
-    console.log(newTodo);
+    const newTodo = await sequelize.query("INSERT INTO todo (firstName, lastName, login, password) VALUES('${firstName}', '${lastName}', '${login}', '${password}') RETURNING *", [firstName, lastName, login, password]);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
