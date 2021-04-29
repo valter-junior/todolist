5287ba0462999862bbd12fdbd5d29918e3b7ed25
Valter Junior <valtrjose.sjr@gmail.com>
2021-04-29 11:48:22 -0300

    Update files

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,77 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
+const jwt = require("jwt-simple");
 const cors = require("cors");
 const pool = require("./db");
 const PORT = process.env.PORT || 3001;
 
-//middleware
-
 app.use(cors());
 app.use(express.json());
+const USER = "admin";
+const PASS = "12345";
+const SECRET = "mysecret";
 
+passport.use(new LocalStrategy((username, password, done) => {
+    if (username === USER && password === PASS){
+      done(null, jwt.encode({username}, SECRET));
+      return;
+    }
+    done(null, false); 
+  }));
 
+app.post('/login', passport.authenticate('local', { session: false }),
+  (req, res) => {
+    res.send({
+      token: req.user,
+    });
+  },
+);
 
+passport.use(new BearerStrategy((token, done) => {
+  try {
+    const { username } = jwt.decode(token, SECRET);
+    if (username === USER) {
+      done(null, username);
+      return;
+    }
+    done(null, false);
+  } catch (error) {
+    done(null, false);
+  }
+}));
 
+app.get('/todos', passport.authenticate('bearer', { session: false }), (req, res, next) => {
+    var user = req.user;
+    req.json(user);       
+  },
+);
+app.post(
+  '/login',
+  passport.authenticate('bearer', { session: false }),
+  (req, res) => {
+    res.send({
+      token: req.user,
+    });
+  },
+);
+app.delete(
+  '/todos/:id',
+  passport.authenticate('bearer', { session: false }),
+  (req, res) => {
+    
+  },
+);
 
 app.post("/sign-in", async (req, res) => {
   try {
     const { firstName, lastName, login, password } = req.body;
     const newTodo = await pool.query("INSERT INTO todo (firstName, lastName, login, password) VALUES($1, $2, $3, $4) RETURNING *", [firstName, lastName, login, password]);
     res.json(newTodo.rows[0]);
-
   } catch (err) {
-    console.error(err.message);
+    console.error(res.json(err.message));
   }
 });
 
@@ -30,7 +81,6 @@ app.get("/sign-in", async (req, res) => {
     res.json(allTodo.rows);
   } catch (err) {
     console.error(err.message);
-    
   }
 });
 
@@ -41,13 +91,10 @@ app.get("/sign-in/:id", async (req, res) => {
     res.json(todo.rows[0]);
   } catch (error) {
     console.error(err.message);
-    
   }
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
